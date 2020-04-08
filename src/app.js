aaaef6ea9b654847a41857516a373ae903f8fcd7
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-08 17:37:02 -0300

    Project CRUD, Likes feature implemented

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,90 @@ app.use(cors());
 
 const repositories = [];
 
+/* Middleware */
+function likesMiddleware(req,res,next){
+  const hasLikes = req.body.likes;
+  if(hasLikes){
+   
+    return res.status(401).json({likes:0});
+  
+  }
+  return next();
+}
+
+app.use(likesMiddleware);
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;
+  const results = title
+    ? repositories.filter(r => r.title.includes(title))
+    : repositories;
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+    
+  }
+  repositories.push(repository)
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const { title, url, techs} = request.body;
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  if(repoIndex < 0){
+    return response.status(400).send();
+  }
+
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes:repositories.likes
+    
+
+    
+    
+  }
+  
+  repositories[repoIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  const repoIndex = repositories.findIndex(r => r.id === id);
+  if(repoIndex < 0){
+    return res.status(400).send();
+  }
+
+  repositories.splice(repoIndex,1);
+
+  return res.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const{ id} = request.params;
+  const repository = repositories.find(repository => repository.id === id);
+  if(!repository){
+    return response.status(400).send();
+  }
+
+
+  repository.likes +=1;
+  return response.json(repository);
 });
 
 module.exports = app;
